cfae896163894368c9a743b6fc60672c064e69f3
Kevin Medina <kmedinakm@hotmail.com>
2020-04-09 23:00:00 -0400

    update post route to create new recipe book

--- routes/recipe-book.routes.js
+++ routes/recipe-book.routes.js
@@ -1,33 +1,36 @@
 // ROUTES FILE NEEDS TO BE REQUIRED IN THE APP.JS IN ORDER NOT TO GIVE 404
 // APP NEEDS TO KNOW YOU CREATED A NEW ROUTE FILE, THAT'S THE ONLY WAY FOR IT TO KNOW WHICH ROUTES YOU WANT TO HIT
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // ********* require Recipe and RecipeBook models in order to use them *********
 // const Recipe = require('../models/Recipe.model');
-const RecipeBook = require('../models/RecipeBook.model');
+const RecipeBook = require("../models/RecipeBook.model");
 
 // ****************************************************************************************
 // POST - create a recipe book
 // ****************************************************************************************
 
 // <form action="/recipe-books" method="POST">
-router.post('/recipe-books', (req, res) => {
+router.post("/new-recipebook", (req, res) => {
   // console.log(req.body);
-  RecipeBook.create(req.body)
-    .then(RecipeBook => res.status(200).json({ book: RecipeBook }))
-    .catch(err => next(err));
+  const { param } = req.body;
+  RecipeBook.create({ title: param, author: req.user._id })
+    .then((RecipeBook) => res.status(200).json({ RecipeBook }))
+    .catch((err) => next(err));
 });
 
 // ****************************************************************************************
 // GET route to get all the recipe books
 // ****************************************************************************************
 
-router.get('/recipe-books', (req, res) => {
+router.get("/recipe-books", (req, res) => {
   RecipeBook.find()
-    .then(recipeBooksFromDB => res.status(200).json({ books: recipeBooksFromDB }))
-    .catch(err => next(err));
+    .then((recipeBooksFromDB) =>
+      res.status(200).json({ books: recipeBooksFromDB })
+    )
+    .catch((err) => next(err));
 });
 
 // ****************************************************************************************
@@ -35,10 +38,10 @@ router.get('/recipe-books', (req, res) => {
 // ****************************************************************************************
 
 // <form action="/recipe-books/{{this._id}}/delete" method="post">
-router.post('/recipe-books/:recipeBookId/delete', (req, res) => {
+router.post("/recipe-books/:recipeBookId/delete", (req, res) => {
   RecipeBook.findByIdAndRemove(req.params.recipeBookId)
-    .then(() => res.json({ message: 'Successfully removed the recipe book!' }))
-    .catch(err => next(err));
+    .then(() => res.json({ message: "Successfully removed the recipe book!" }))
+    .catch((err) => next(err));
 });
 
 // ****************************************************************************************
@@ -46,21 +49,23 @@ router.post('/recipe-books/:recipeBookId/delete', (req, res) => {
 // ****************************************************************************************
 
 // <form action="/recipe-books/{{foundBook._id}}/update" method="POST">
-router.post('/recipe-books/:recipeBookId/update', (req, res) => {
+router.post("/recipe-books/:recipeBookId/update", (req, res) => {
   RecipeBook.findByIdAndUpdate(req.params.recipeBookId, req.body, { new: true })
-    .then(updatedRecipeBook => res.status(200).json({ book: updatedRecipeBook }))
-    .catch(err => next(err));
+    .then((updatedRecipeBook) =>
+      res.status(200).json({ book: updatedRecipeBook })
+    )
+    .catch((err) => next(err));
 });
 
 // ****************************************************************************************
 // GET route for getting the recipe book details
 // ****************************************************************************************
 
-router.get('/recipe-books/:recipeBookId', (req, res) => {
+router.get("/recipe-books/:recipeBookId", (req, res) => {
   RecipeBook.findById(req.params.recipeBookId)
-    .populate('recipe')
-    .then(RecipeBook => res.status(200).json({ book: RecipeBook }))
-    .catch(err => next(err));
+    .populate("recipe")
+    .then((RecipeBook) => res.status(200).json({ book: RecipeBook }))
+    .catch((err) => next(err));
 });
 
 module.exports = router;
