c4eece9bc2202ad49049006e80889ec082c33de7
Kevin Medina <kmedinakm@hotmail.com>
2020-04-22 23:31:34 -0400

    update the delete route to send resonse and message back

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -59,7 +59,9 @@ recipeRouter.post("/recipe/delete", (req, res, next) => {
       Recipe.findByIdAndRemove(recipeID)
         .then((deletedRecipe) => {
           console.log("Deleted recipe: ", deletedRecipe);
-          res.status(200).json({ message: "Recipe successfully deleted!" });
+          res
+            .status(200)
+            .json({ deletedRecipe, message: "Recipe successfully deleted!" });
         })
         .catch((err) =>
           console.log("Error while finding the recipe to delete: ", err)
