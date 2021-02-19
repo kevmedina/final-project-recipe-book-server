21a26ad00f3671093ab34c3971a46900fed65bc3
Kevin Medina <kmedinakm@hotmail.com>
2021-02-18 20:12:45 -0500

    update the avatar image for the user's default image

--- models/User.model.js
+++ models/User.model.js
@@ -1,33 +1,34 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
     username: {
       type: String,
       trim: true,
-      required: [true, 'Username is required.'],
-      unique: true
+      required: [true, "Username is required."],
+      unique: true,
     },
     email: {
       type: String,
-      required: [true, 'Email is required.'],
-      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+      required: [true, "Email is required."],
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
     passwordHash: {
       type: String,
-      required: [true, 'Password is required.']
+      required: [true, "Password is required."],
     },
     image: {
       type: String,
-      default: 'https://www.cetaad.org/sites/default/files/inline-images/avatar-placeholder.png'
-    }
+      default:
+        "https://lh3.googleusercontent.com/proxy/mY819mIdue_DtALuZUkmehqO49bAzg-Cfboqd-MWPBsxnhAtbsmZdhLbNtyiYfeWWjJp7M0Cj1HFZkZ6yOcpljrWKRWQ3t_rNJVLlVOWcHTNoNsH3Qb4TQ",
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
 
-module.exports = model('User', userSchema);
+module.exports = model("User", userSchema);
